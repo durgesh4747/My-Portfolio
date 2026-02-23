b8aea084529366a1aadf1e4531fe2f56e63746b2
durgesh4747 <durgeshsutariya07@gmail.com>
2026-02-23 18:44:28 +0530

    sitemap.xml built

---
+++ portfolio/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://durgeshdev.in',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1, 
+    },
+    {
+      url: 'https://durgeshdev.in/inventory',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5, 
+    },
+  ]
+}
