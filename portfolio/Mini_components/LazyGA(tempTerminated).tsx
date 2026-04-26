89befcf2b66805a820eaf81a190ea9e21aea6e77
durgesh4747 <durgeshsutariya07@gmail.com>
2026-04-26 09:27:36 +0530

    Removed google analytics temporary - due to performance issues - when have time then  reoptimizing the site change it with Partytown/Worker Strategy

--- portfolio/Mini_components/LazyGA(tempTerminated).tsx
+++ portfolio/Mini_components/LazyGA(tempTerminated).tsx
@@ -10,16 +10,13 @@ export default function LazyGA({ gaId }: LazyGAProps) {
   const [shouldLoad, setShouldLoad] = useState(false);
 
   useEffect(() => {
-    // List of events that signal a real human interaction
     const loginEvents = ["mouseover", "keydown", "touchstart", "scroll"];
 
     const initGA = () => {
       setShouldLoad(true);
-      // Clean up listeners once GA is triggered
       loginEvents.forEach((event) => window.removeEventListener(event, initGA));
     };
 
-    // Listeners
     loginEvents.forEach((event) =>
       window.addEventListener(event, initGA, { passive: true }),
     );
